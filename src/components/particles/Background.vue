20bcf607b8c8248f8af608a19417f59d360eea69
Uwith <wantmoononelife@outlook.com>
2021-07-15 08:52:09 +0800

    背景

---
+++ src/components/particles/Background.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="min-height:730px;height: 98.5vh" class="">
+    <!--  <div style="min-height:730px; height:960px" class="">-->
+    <div id="background">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+import particlesConfig from "./Background.json";
+
+export default {
+  name: "Background",
+  data() {
+    return {};
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      particlesJS("background", particlesConfig);
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+@import "./style.css";
+</style>
---
+++ src/components/particles/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('background', 'assets/Background.json', function() {
+    console.log('callback - particles.js config loaded');
+});
