4c21dc3e1d68691ab006cfc765602862aae1cb52
Uwith <wantmoononelife@outlook.com>
2021-07-18 15:05:21 +0800

    测试页

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mian">
+    <Background/>
+  </div>
+</template>
+
+<script>
+import Background from "@/components/particles/Background";
+
+export default {
+  name: "Main",
+  components: {Background}
+}
+</script>
+
+<style scoped>
+
+</style>
