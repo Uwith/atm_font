f564fd1d2c3e9ce952fe213976c050c0dfac9b69
Uwith <wantmoononelife@outlook.com>
2021-07-18 15:04:14 +0800

    路由更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import Login from "@/views/Login";
+import SaveMoney from "@/views/SaveMoney";
+import WithdrawMoney from "@/views/WithdrawMoney";
+import SaveMoneyNext from "@/views/SaveMoneyNext";
+import Language from "@/views/Language";
+import Cancellation from "@/views/Cancellation";
+import Balance from "@/views/Balance";
+import Agreement from "@/views/Agreement";
+import Background from "@/components/particles/Background";
+import Register from "@/views/Register";
 
 Vue.use(VueRouter)
 
@@ -11,13 +21,70 @@ const routes = [
         component: Home
     },
     {
-        path: '/about',
-        name: 'About',
+        path: '/agreement',
+        name: 'Agreement',
+        component: Agreement
+    },
+    {
+        path: '/balance',
+        name: 'Balance',
+        component: Balance
+    },
+    {
+        path: '/cancellation',
+        name: 'Cancellation',
+        component: Cancellation
+    },
+    {
+        path: '/history',
+        name: 'History',
+        component: History
+    },
+    {
+        path: '/language',
+        name: 'Language',
+        component: Language
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/saveMoney',
+        name: 'SaveMoney',
+        component: SaveMoney
+    },
+    {
+        path: '/saveMoneyNext',
+        name: 'SaveMoneyNext',
+        component: SaveMoneyNext
+    },
+    {
+        path: '/withdrawMoney',
+        name: 'WithdrawMoney',
+        component: WithdrawMoney
+    },
+
+    {
+        path: '/background',
+        name: 'Background',
+        component: Background
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    },
+
+    {
+        path: '/error',
+        name: 'Error',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+        component: () => import(/* webpackChunkName: "about" */ '../views/Error.vue')
+    },
 ]
 
 const router = new VueRouter({
