aac9ba10ff6d09feec7351bcae8020f07f6e4138
Uwith <wantmoononelife@outlook.com>
2021-07-22 17:57:52 +0800

    添加工具类

--- src/main.js
+++ src/main.js
@@ -7,15 +7,46 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './css/public.css';
 import axios from 'axios';
+import API from '@/utils/APIUtil';
+import fetch from '@/utils/fetch'
+import Storage from '@/utils/StorageUtil'
 
+//阻止启动生产消息
 Vue.config.productionTip = false
-Vue.use(particles)
+// 全局挂在组件
+const extend = Vue.prototype
 
-Vue.use(ElementUI);
+//定义全局变量
+Vue.prototype.Storage = Storage;
+Vue.prototype.API = API;
+
+extend.$post = fetch.post
+extend.$get = fetch.get
+extend.$form = fetch.form
+
+
+Vue.use(ElementUI)
+//路由拦截
+// router.beforeEach(function (to, from, next) {
+//   var user = Storage.Session.get('User');
+//   if (!user) {
+//     if (to.path.indexOf('/views') != -1) {
+//       router.push({
+//         path: '/login',
+//         name: 'Login',
+//         params: {
+//           msg: '您还未登录，请登录！'
+//         }
+//       })
+//     }
+//   }
+//   next(true)
+// })
 
 new Vue({
-    router,
-    store,
-    axios,
-    render: h => h(App)
+  router,
+  particles,
+  store,
+  axios,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/utils/APIUtil.js
@@ -0,0 +1,30 @@
+﻿var serverBase = 'http://127.0.0.1:8081/'
+// var v1 = serverBase + '/api/v1/'
+var v1 = serverBase + 'atm/'
+export default {
+  BASE_SERVER_URL: serverBase,
+  /**
+   * 登录
+   * post
+   * @Param  userkeyCustomer passkeyCustomer
+   */
+  API_URL_LOGIN: serverBase + "login",
+  /**
+   * 存取款接口
+   * post
+   * @Param cardId doMoney doType
+   */
+  API_URL_DO_MONEY: v1 + "user/doMoney",
+  /**
+   * 查询余额
+   * get
+   * @param cardId
+   */
+  API_URL_BALANCER: v1 + "allQuery/balance",
+  /**
+   * 查询银行卡流水
+   * post
+   * @param cardId
+   */
+  API_URL_CARD_LOG: v1 + "allQuery/cardLog",
+}
