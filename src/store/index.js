392fdae9f3ec3cc9d28a82933a984a36842b86bc
Uwith <wantmoononelife@outlook.com>
2021-07-22 17:58:53 +0800

    完善状态管理

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+const extend = Vue.prototype
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
-    modules: {}
+  state: {
+    token: '',
+    keyword: '',
+    user: null,
+    cartCount: null,
+    cartProductId: null
+  },
+  mutations: {
+    setToken(state, token) {
+      state.token = token
+    },
+    setKeyword(state, keyword) {
+      state.keyword = keyword
+    },
+    setUser(state, user) {
+      state.user = user
+    },
+    setCartCount(state, cartCount) {
+      state.cartCount = cartCount
+    },
+    setCartProductId(state, productId) {
+      state.cartProductId = productId
+    }
+  },
+  actions: {},
+  modules: {}
 })
+
+// sessionStorage.setItem('token', res.data.data)
