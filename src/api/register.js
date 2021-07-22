107429757c7be81053085fa0f73ec838b3596a3b
Uwith <wantmoononelife@outlook.com>
2021-07-22 18:00:21 +0800

    api重写

--- src/api/register.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export function login(user) {
-    return axios({
-        method: 'post',
-        url: 'http://127.0.0.1:8081/login/in',
-        data: user
-    })
-}
