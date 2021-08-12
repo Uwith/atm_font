ea7641a2b5032622a4c8e0b47c7cc303e6d73bf3
Uwith <wantmoononelife@outlook.com>
2021-08-12 10:53:58 +0800

    配置文件修改

--- src/router/index.js
+++ src/router/index.js
@@ -89,7 +89,6 @@ const routes = [
     name: 'CardLogin',
     component: CardLogin
   },
-
   {
     path: '/error',
     name: 'Error',
@@ -101,7 +100,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
